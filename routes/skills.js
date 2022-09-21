91e5fd4430b1fd1e1d129e840a248365b0c5b321
GFou <gfouchard20@outlook.com>
2022-09-21 17:50:55 -0400

    move skills import from route to controllers

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 /* GET skills listing. */
-router.get('/',)
+router.get('/', skillsCtrl.index)
 
 export {
   router
