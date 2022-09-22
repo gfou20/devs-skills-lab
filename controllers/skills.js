a18080a4a0ae4df4d07bcd073fbb55eb8477ad25
GFou <gfouchard20@outlook.com>
2022-09-22 16:46:33 -0400

    create views/skills/show.ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,7 +17,33 @@ function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res) {
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
   newSkill as new,
+  create,
+  show,
 }
